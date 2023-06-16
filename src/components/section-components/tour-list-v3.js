0bee918e7fa446001af2e998cbd1e83e17fb3793
MrBlackGhostt <hemantkumar4213@icloud.com>
2023-06-16 22:18:42 +0530

    Add search feature in the all tour page

--- src/components/section-components/tour-list-v3.js
+++ src/components/section-components/tour-list-v3.js
@@ -1,13 +1,59 @@
-import React, { Component } from "react";
-import ImageLazyLoad from "../section-components/ImageLazyLoad";
-import { Link } from "react-router-dom";
-import parse from "html-react-parser";
-
+import React, { Component, useState } from "react"
+import ImageLazyLoad from "../section-components/ImageLazyLoad"
+import { Link } from "react-router-dom"
+import parse from "html-react-parser"
 
 class TourListV3 extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      query: "",
+      locationFilter: "",
+      priceFilter: null,
+    }
+  }
+
+  handleSearch = (e) => {
+    this.setState({ query: e.target.value })
+  }
+
+  handleLocationFilter = (e) => {
+    this.setState({ locationFilter: e.target.value })
+  }
+
+  handlePriceFilter = (e) => {
+    const priceFilter = e.target.value
+    console.log(priceFilter, "e.targf")
+    this.setState({ priceFilter })
+  }
+
   renderContent() {
-    let publicUrl = process.env.PUBLIC_URL + "/";
-    return this.props.data.map((singleContent) => {
+    const { query, locationFilter, priceFilter } = this.state
+
+    const filteredData = this.props.data.filter((singleContent) => {
+      const parsedPriceFilter = parseFloat(priceFilter)
+      const parsedPrice = parseFloat(singleContent.price)
+      const lowercaseQuery = query.toLowerCase()
+      const lowercaseLocation = singleContent.location.toLowerCase()
+
+      console.log(parsedPrice, parsedPriceFilter)
+
+      return (
+        (singleContent.name.toLowerCase().includes(lowercaseQuery) ||
+          singleContent.name === "") &&
+        (locationFilter === "" ||
+          lowercaseLocation.includes(locationFilter.toLowerCase())) &&
+        (priceFilter === null || parsedPrice <= parsedPriceFilter)
+      )
+    })
+
+    filteredData.sort((a, b) => {
+      return parseInt(b.price) - parseInt(a.price)
+    })
+
+    let publicUrl = process.env.PUBLIC_URL + "/"
+    return filteredData.map((singleContent) => {
+      console.log(singleContent)
       const {
         imagewebp,
         imagejpeg,
@@ -17,143 +63,131 @@ class TourListV3 extends Component {
         name,
         link,
         price,
-      } = singleContent;
+      } = singleContent
+
       return (
-        <div className="col-lg-4 col-sm-6">
-          <div className="single-destinations-list style-two">
+        <div className='col-lg-4 col-sm-6'>
+          <div className='single-destinations-list style-two'>
             <Link to={link}>
-              <div className="thumb">
+              <div className='thumb'>
                 <ImageLazyLoad
                   imagejpeg={imagejpeg}
                   imagewebp={imagewebp}
                   alt={alt}
                 />
               </div>
-            
-              <div className="details">
-                <p className="location">
+
+              <div className='details'>
+                <p className='location'>
                   <img
                     src={
                       "https://delhibycycle.s3.ap-south-1.amazonaws.com/1.png"
                     }
-                    alt="map"
+                    alt='map'
                   />
                   {location}
                 </p>
-                <h4 className="title">
+                <h4 className='title'>
                   <Link to={link}>{name}</Link>
                 </h4>
-                <p className="content">{duration}</p>
-                <div className="tp-price-meta">
+                <p className='content'>{duration}</p>
+                <div className='tp-price-meta'>
                   <h2>INR {price}</h2>
                 </div>
               </div>
             </Link>
           </div>
         </div>
-      );
-    });
+      )
+    })
   }
   render() {
-    let publicUrl = process.env.PUBLIC_URL + "/";
-    let imagealt = "image";
+    let publicUrl = process.env.PUBLIC_URL + "/"
+    let imagealt = "image"
+    let { query, locationFilter, priceFilter } = this.state
 
     return (
-      <div className="tour-list-area pd-top-120 viaje-go-top">
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-9 col-lg-8 order-lg-12">
-              <div className="tp-tour-list-search-area">
-                <div className="row">
-                  <div className="col-xl-3 col-sm-6">
-                    <label className="single-input-wrap">
-                      <i className="fa fa-calendar-minus-o" />
-                      <input
-                        type="text"
-                        className="departing-date"
-                        placeholder="Departing"
-                      />
-                    </label>
-                  </div>
-                  <div className="col-xl-3 col-sm-6">
-                    <label className="single-input-wrap tour-list-search-icon">
-                      <i className="la la-arrow-up" />
-                      <input type="text" placeholder="Price Low to High" />
-                    </label>
-                  </div>
-                  <div className="col-xl-3 col-sm-6">
-                    <label className="single-input-wrap tour-list-search-icon">
-                      <i className="la la-arrow-down" />
-                      <input type="text" placeholder="Price High to Low" />
-                    </label>
-                  </div>
-                  <div className="col-xl-3 col-sm-6">
-                    <label className="single-input-wrap">
-                      <i className="fa fa-paper-plane" />
-                      <input type="text" placeholder="Name (A - Z)" />
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <div className="row justify-content-center">
+      <div className='tour-list-area pd-top-120 viaje-go-top'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-xl-9 col-lg-8 order-lg-12'>
+              <div className='row justify-content-center'>
                 {this.renderContent()}
               </div>
             </div>
-            <div className="col-xl-3 col-lg-4 order-lg-1">
-              <div className="sidebar-area sidebar-area-inner-page">
-                <div className="widget tour-list-widget">
-                  <div className="widget-tour-list-search">
-                    <form className="search-form">
-                      <div className="form-group">
-                        <input type="text" placeholder="Search" />
+            <div className='col-xl-3 col-lg-4 order-lg-1'>
+              <div className='sidebar-area sidebar-area-inner-page'>
+                <div className='widget tour-list-widget'>
+                  <div className='widget-tour-list-search'>
+                    <form className='search-form'>
+                      <div className='form-group'>
+                        <input
+                          type='text'
+                          value={query}
+                          onChange={this.handleSearch}
+                          placeholder='Search'
+                        />
                       </div>
-                      <button className="submit-btn" type="submit">
-                        <i className="ti-search" />
+                      <button className='submit-btn' type='submit'>
+                        <i className='ti-search' />
                       </button>
                     </form>
                   </div>
-                  <div className="widget-tour-list-meta">
+                  <div className='widget-tour-list-meta'>
                     {/* <div className="single-widget-search-input-title">
                       <i className="fa fa-dot-circle-o" /> Location
                     </div> */}
                     {/* <div className="single-widget-search-input">
                       <input type="text" placeholder="Tour List Destination" />
                     </div> */}
-                    <div className="single-widget-search-input-title">
-                      <i className="fa fa-plus-circle" /> Location
+                    <div className='single-widget-search-input-title'>
+                      <i className='fa fa-plus-circle' /> Location
                     </div>
-                    <div className="single-widget-search-input">
-                      <select className="select w-100 custom-select">
-                        <option value={1}>Old Delhi</option>
-                        <option value={2}>New Delhi</option>
-                        <option value={3}>Agra</option>
-                        <option value={4}>Rajasthan</option>
-                        <option value={5}>Goa</option>
+                    <div className='single-widget-search-input'>
+                      <select
+                        className='select w-100 custom-select'
+                        value={locationFilter}
+                        onChange={this.handleLocationFilter}
+                      >
+                        <option value={"Old Delhi"}>Old Delhi</option>
+                        <option value={"New Delhi"}>New Delhi</option>
+                        <option value={"Agra"}>Agra</option>
+                        <option value={"Rajashthan"}>Rajasthan</option>
+                        <option value={"Goa"}>Goa</option>
                       </select>
                     </div>
                     {/* <div className="single-widget-search-input">
                       <input type="text" placeholder="Tour List Destination" />
                     </div> */}
-                    <div className="single-widget-search-input-title">
-                      <i className="fa fa-plus-circle" /> Travel Type
+                    <div className='single-widget-search-input-title'>
+                      <i className='fa fa-plus-circle' /> Travel Type
                     </div>
-                    <div className="single-widget-search-input">
-                      <select className="select w-100 custom-select">
+                    <div className='single-widget-search-input'>
+                      <select className='select w-100 custom-select'>
                         <option value={1}>Day Cycle Tour</option>
                         <option value={2}>Walking Tour</option>
                         <option value={3}>Cycling Holidays</option>
                       </select>
                     </div>
 
-                    <div className="single-widget-search-input-title">
-                      <i className="fa fa-usd" /> Price Filter
+                    <div className='single-widget-search-input-title'>
+                      <i className='fa fa-usd' /> Price Filter
                     </div>
-                    <div className="widget-product-sorting">
-                      <div className="slider-product-sorting" />
+                    <div className='widget-product-sorting'>
+                      <select
+                        className='select w-100 custom-select'
+                        value={priceFilter}
+                        onChange={this.handlePriceFilter}
+                      >
+                        <option value={3000}> Under 3000</option>
+                        <option value={20000}> 3000 - 20000</option>
+                        <option value={75001}>Above 20000 </option>
+                      </select>
+                      {/* <div className="slider-product-sorting" />
                       <div className="product-range-detail">
                         <label htmlFor="amount">Price: </label>
                         <input type="text" id="amount" readOnly />
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 </div>
@@ -162,8 +196,8 @@ class TourListV3 extends Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default TourListV3;
+export default TourListV3
